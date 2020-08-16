7edf5ca4fb741e753fa872a98974f1c6882254fe
Sujeet Kumar <M1059025@mindtree.com>
2020-08-16 07:43:18 +0530

    module1

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+var fs = require('fs');
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.render("index", {
+        title: "Index"
+    });
+});
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(port, '127.0.0.1', 511, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+// app.listen(3000,'0.0.0.0',function(){
+//   console.log('Server running at http://127.0.1.1:3000/')
+// });
+
+// app.listen(port, "localhost", function(error) {
+//   if (error) {
+//     console.error("Unable to listen on port", port, error);
+//     listen(port + 1);
+//     return;
+//   }
+//   console.log('Server running at http://127.0.1.1:3000/');
+// });
